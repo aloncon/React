80ebb11917e6bdae2bf444a1c10768e16c42c7ee
Alon Cohen <aloncon@gmail.com>
2019-11-06 10:50:01 +0200

    add trade-shows json object

--- elcogem/src/Containers/TradeShows/TradeShows.js
+++ elcogem/src/Containers/TradeShows/TradeShows.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import tradeShowList from '../../ModuleInfo/trade_shows';
+const TradeShows = (props) => {
+    console.log("trade Objext " , tradeShowList);
 
-const TradeShows = () => {
+    for(let key in tradeShowList){
+        if(tradeShowList.hasOwnProperty(key)){
+            console.log("trade list",tradeShowList[key]);
+        }
+    }
+
+    const tradeItems = Object.keys(tradeShowList)
+        .map((tradeKey)=>{
+            return (
+            <div key={tradeKey} style={{backgroundColor:'#c1c1c1',color:'white', width:'70%',padding:'10px', margin:'10px'}}>
+                <div>{tradeShowList[tradeKey].titleMain}</div>
+                <div>{tradeShowList[tradeKey].titleSub}</div>
+                <div>{tradeShowList[tradeKey].Boot}</div>
+                <div>{tradeShowList[tradeKey].coments}</div>
+
+            </div>);
+        });
+
+
     return (
         <div>
             TradeShows ....
+            <div>
+                {tradeItems}
+            </div>
         </div>
     )
 }
---
+++ elcogem/src/ModuleInfo/trade_shows.js
@@ -0,0 +1,23 @@
+const tradeShowsList = {
+    "tradeShow1" : {
+        active: true,
+        titleMain: "The JCK Show, Las Vegas",
+        titleSub: "FRI, MAY 31 – MON, JUNE 3, 2019",
+        Boot: "Booth Number: 51012",
+        coments: "SANDS EXPO & THE VENETIAN | LAS VEGAS, NV",
+        mainImage: "images/ELCOGEM-Trade-Showe-web.jpg",
+        mainImageTitle: "You are invited to view our newest collections at JCK Las Vegas."
+    },
+    "tradeShow2" : {
+        active: true,
+        titleMain: "AGTA Gemfair Tucson",
+        titleSub: "FRI, MAY 31 – MON, JUNE 3, 2019",
+        Boot: null,
+        coments: "January 31- February 4, 2019",
+        mainImage: null,
+        mainImageTitle: null
+    },
+
+}
+
+export default tradeShowsList;
